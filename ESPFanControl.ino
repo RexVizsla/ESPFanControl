16a51370c6297d1abc2febe60844001b82566d55
Rex Vizsla <rexvizsla@gmail.com>
2024-10-31 22:19:56 +0100

    Added OTA updates

--- ESPFanControl.ino
+++ ESPFanControl.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <ArduinoOTA.h>
 #include "config.h"
 
 // Global objects
@@ -48,10 +49,45 @@ void setup() {
   // Setup WiFi and MQTT
   setupWifi();
   client.setServer(MQTT_SERVER, MQTT_PORT);
+
+  // Initialize OTA
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
 void loop() {
   unsigned long currentMillis = millis();
+
+    // Handle OTA updates
+  ArduinoOTA.handle();
 
   // Reconnect MQTT if needed
   if (!client.connected()) {
